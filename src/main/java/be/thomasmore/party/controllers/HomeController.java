e9519139c7fae9bb81639d1f777cc16d0fe8fa29
InesEmily <inesemilymertens@gmail.com>
2022-02-18 09:34:00 +0100

    Database

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,17 +1,40 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller // zegt dat deze klassen een controller
 public class HomeController {
 
     @GetMapping({"/","/home"})
-    public String home(){
+    public String home(Model model){
+        int myCalculatedValue = 34*62;
+        model.addAttribute("myCalculatedValue",myCalculatedValue);
+
         return "home";
     }
     @GetMapping("/about")
-    public String about(){
+    public String about(Model model){
+        String naam = "Anne Van Goethem";
+        String adres = "Koning Albertlaan 23";
+        String stad = "Leuven";
+        model.addAttribute("naam",naam);
+        model.addAttribute("adres",adres);
+        model.addAttribute("stad",stad);
         return "about";
+    }
+    @GetMapping("/pay")
+    public String date(Model model){
+        LocalDate date = LocalDate.now();
+        LocalDate date30 = date.plusDays(30);
+        model.addAttribute("date",date);
+        model.addAttribute("date30",date30);
+
+        return "pay";
     }
 }
---
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -0,0 +1,16 @@
+package be.thomasmore.party.controllers;
+
+import be.thomasmore.party.model.Venue;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VenueController {
+    @GetMapping({"/venuedetails"})
+    public String venue(Model model){
+        Venue v = new Venue("Boesj","https://www.facebook.com/boesjkammeree/",100,false,true,false,false,"Mechelen",1);
+        model.addAttribute("venue",v);
+        return "venue";
+    }
+}
