6139819a546171eb9498e538f13cf8d0f737cb7c
ines <r0306129@student.thomasmore.be>
2022-03-22 15:27:26 +0100

    bundel 5 afgewerkt

--- src/main/java/be/thomasmore/party/controllers/AdminController.java
+++ src/main/java/be/thomasmore/party/controllers/AdminController.java
@@ -3,9 +3,11 @@ package be.thomasmore.party.controllers;
 import be.thomasmore.party.model.Party;
 import be.thomasmore.party.model.Venue;
 import be.thomasmore.party.repositories.PartyRepository;
+import be.thomasmore.party.repositories.VenueRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,40 +21,48 @@ public class AdminController {
     private Logger logger = LoggerFactory.getLogger(AdminController.class);
     @Autowired
     private PartyRepository partyRepository;
+    @Autowired
+    private VenueRepository venueRepository;
 
     @GetMapping({"/partyedit/{id}"})
     public String partyedit(Model model, @PathVariable(required = false) Integer id) {
-        if (id == null) return "/admin/partyedit";
-
-
-        Optional<Party> partyFromDb = partyRepository.findById(id);
-
-        if (partyFromDb.isPresent()) {
-            model.addAttribute("party", partyFromDb.get());
-            logger.info(partyFromDb.get().getName());
-//            long maxvenues = partyRepository.count();
-//            model.addAttribute("maxvenues",maxvenues);
-
-        }
+        logger.info("partyEdit" + id);
+        final Iterable<Venue> venues = venueRepository.findAll();
+        model.addAttribute("venues", venues);
         return "/admin/partyedit";
     }
 
     @PostMapping("/partyedit/{id}")
-    public String partyEditPost(Model model, @PathVariable(required = false) Integer id, @ModelAttribute("party") Party party) {
+    public String partyEditPost(Model model, @PathVariable Integer id, @ModelAttribute("party") Party party) {
         logger.info("partyEditPost" + id);
-        Optional<Party> optionalParty = partyRepository.findById(id);
-        if (optionalParty.isPresent()) {
-
-            Party editParty = optionalParty.get();
-
-            editParty.setName(party.getName());
-            editParty.setPriceInEur(party.getPriceInEur());
-            editParty.setPricePresaleInEur(party.getPricePresaleInEur());
-            editParty.setExtraInfo(party.getExtraInfo());
-            partyRepository.save(editParty);
-            model.addAttribute("party", party);
-        }
+        partyRepository.save(party);
+        model.addAttribute("party", party);
         return "redirect:/partydetails/" + id;
+    }
+
+    @ModelAttribute("party")
+    public Party findparty(@PathVariable(required = false) Integer id) {
+        logger.info("findParty" + id);
+        if (id == null) {
+            return new Party();
+        }
+        Optional<Party> optionalParty = partyRepository.findById(id);
+        if (optionalParty.isPresent())
+            return optionalParty.get();
+        return null;
+    }
+
+    @GetMapping({"/partynew"})
+    public String partynew(Model model) {
+        final Iterable<Venue> venues = venueRepository.findAll();
+        model.addAttribute("venues", venues);
+        return "admin/partynew";
+    }
+    @PostMapping("/partynew")
+    public String partyNewPost(Model model,@ModelAttribute("party")Party party){
+        partyRepository.save(party);
+        int id = partyRepository.save(party).getId();
+        return "redirect:/partydetails"+id;
     }
 }
 
--- src/main/java/be/thomasmore/party/repositories/PartyRepository.java
+++ src/main/java/be/thomasmore/party/repositories/PartyRepository.java
@@ -4,10 +4,15 @@ import be.thomasmore.party.model.Party;
 import be.thomasmore.party.model.Venue;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.criteria.CriteriaBuilder;
 import java.util.Optional;
 
 public interface PartyRepository extends CrudRepository<Party, Integer> {
 
     Iterable<Party>findByVenue(Optional<Venue> venue);
+
 }
